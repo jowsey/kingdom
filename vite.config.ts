da55396e5628efe03e3b769ebcfdf4c520c37165
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2024-12-01 05:00:42 +0000

    update deps & optimize lucide build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import {execSync} from 'node:child_process';
+import { execSync } from 'node:child_process';
+import lucidePreprocess from 'vite-plugin-lucide-preprocess';
 
 import pkg from './package.json' with { type: 'json' };
-const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+
+const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [lucidePreprocess(), sveltekit()],
 	define: {
 		__VERSION__: JSON.stringify(pkg.version),
 		__COMMIT__: JSON.stringify(commitHash)
